c9cfe961e2928d43aafce25a9809f26a9ea6620b
Andrey Ponomarenko <slaner2008@yandex.ru>
2019-01-16 10:09:06 +0300

    fix doc

--- dev/modal/docs/items/Options.js
+++ dev/modal/docs/items/Options.js
@@ -40,12 +40,6 @@ class Options extends React.Component {
 							<br/>It's allows you easily to get Kit Modal from any inner elements, which is useful in creating buttons for example. (See Basic demo).
 						</p>
 						<p>Pass false as an option to disable it, or pass the string '.className', to chose elements, where you want to store links.</p>
-						<h3 className={s["h3-border"]}>required</h3>
-						<ul className={s.list}>
-							<li>Type: boolean</li>
-							<li>Default: false</li>
-						</ul>
-						<p>Required window could be closed only using show() method.</p>
 						<h3 className={s["h3-border"]}>absolute</h3>
 						<ul className={s.list}>
 							<li>Type: boolean</li>
